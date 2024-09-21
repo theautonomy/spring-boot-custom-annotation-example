4b863c2d9c0c961abf94f97954d60223062238d2
Wei Li <weili.mail@gmail.com>
2024-09-21 11:40:25 -0400

    auto configuration imports beanregistrar

--- src/main/java/com/example/demo/MyCustomBeanAutoConfiguration.java
+++ src/main/java/com/example/demo/MyCustomBeanAutoConfiguration.java
@@ -4,5 +4,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 @Configuration
-@Import(MyCustomBeanImportSelector.class)
+
+// Both ways work
+// @Import(MyCustomBeanImportSelector.class)
+
+@Import(MyCustomBeanRegistrar.class)
 public class MyCustomBeanAutoConfiguration {}
