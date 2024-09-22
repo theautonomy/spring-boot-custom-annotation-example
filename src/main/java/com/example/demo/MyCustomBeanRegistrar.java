7339218cebf7f675a2fb40612ac939d397760cbc
Wei Li <weili.mail@gmail.com>
2024-09-21 22:27:05 -0400

    Update MyCustomBeanRegistrar using a differnet approach

--- src/main/java/com/example/demo/MyCustomBeanRegistrar.java
+++ src/main/java/com/example/demo/MyCustomBeanRegistrar.java
@@ -1,38 +1,39 @@
 package com.example.demo;
 
-import java.util.Set;
-
-import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.beans.factory.support.BeanDefinitionRegistry;
 import org.springframework.context.annotation.ClassPathScanningCandidateComponentProvider;
 import org.springframework.context.annotation.ImportBeanDefinitionRegistrar;
 import org.springframework.core.type.AnnotationMetadata;
 import org.springframework.core.type.filter.AnnotationTypeFilter;
 
+import java.util.Set;
+
 public class MyCustomBeanRegistrar implements ImportBeanDefinitionRegistrar {
 
     @Override
-    public void registerBeanDefinitions(
-            AnnotationMetadata importingClassMetadata, BeanDefinitionRegistry registry) {
-        // Create scanner and set filter for @MyCustomBean annotation
-        ClassPathScanningCandidateComponentProvider scanner =
-                new ClassPathScanningCandidateComponentProvider(false);
+    public void registerBeanDefinitions(AnnotationMetadata importingClassMetadata, BeanDefinitionRegistry registry) {
+        ClassPathScanningCandidateComponentProvider scanner = new ClassPathScanningCandidateComponentProvider(false);
         scanner.addIncludeFilter(new AnnotationTypeFilter(MyCustomBean.class));
 
-        // Scan the base package
-        Set<BeanDefinition> beanDefinitions = scanner.findCandidateComponents("com.example");
+        Set<org.springframework.beans.factory.config.BeanDefinition> beanDefinitions = scanner.findCandidateComponents("com.example");
 
-        for (BeanDefinition beanDefinition : beanDefinitions) {
+        for (org.springframework.beans.factory.config.BeanDefinition beanDefinition : beanDefinitions) {
             try {
                 Class<?> beanClass = Class.forName(beanDefinition.getBeanClassName());
                 MyCustomBean myCustomBean = beanClass.getAnnotation(MyCustomBean.class);
-                String beanName =
-                        myCustomBean.value().isEmpty()
-                                ? beanClass.getSimpleName()
-                                : myCustomBean.value();
-
-                // Register bean
-                registry.registerBeanDefinition(beanName, beanDefinition);
+                String beanName = myCustomBean.value().isEmpty() ? beanClass.getSimpleName() : myCustomBean.value();
+                
+                // Use AbstractBeanDefinition and set a supplier to use BeanFactory
+                org.springframework.beans.factory.support.AbstractBeanDefinition abd = 
+                    (org.springframework.beans.factory.support.AbstractBeanDefinition) beanDefinition;
+                abd.setInstanceSupplier(() -> {
+                    try {
+                        return beanClass.getDeclaredConstructor().newInstance();
+                    } catch (Exception e) {
+                        throw new RuntimeException("Failed to create an instance of class " + beanClass.getName(), e);
+                    }
+                });
+                registry.registerBeanDefinition(beanName, abd);
             } catch (ClassNotFoundException e) {
                 e.printStackTrace();
             }
